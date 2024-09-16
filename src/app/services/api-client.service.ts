93f4781dbc55536bb9e0a9020cf9cfd659606ef3
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-16 09:57:25 +0200

    implement caches

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,18 +9,36 @@ import { catchError, retry } from 'rxjs/operators';
 export class ApiClientService {
   private apiUrl = 'https://jsonplaceholder.typicode.com';
 
+  private cache: { [key: string]: any } = {};
+
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<any> {
-    return this.http
-      .get(`${this.apiUrl}/posts`)
-      .pipe(retry(2), catchError(this.handleError));
+    const cacheKey = 'posts';
+
+    if (this.cache[cacheKey]) {
+      return of(this.cache[cacheKey]);
+    }
+
+    return this.http.get(`${this.apiUrl}/posts`).pipe(
+      retry(2),
+      tap((data) => (this.cache[cacheKey] = data)),
+      catchError(this.handleError)
+    );
   }
 
   getPost(id: number): Observable<any> {
-    return this.http
-      .get(`${this.apiUrl}/posts/${id}`)
-      .pipe(retry(2), catchError(this.handleError));
+    const cacheKey = `post-${id}`;
+
+    if (this.cache[cacheKey]) {
+      return of(this.cache[cacheKey]);
+    }
+
+    return this.http.get(`${this.apiUrl}/posts/${id}`).pipe(
+      retry(2),
+      tap((data) => (this.cache[cacheKey] = data)),
+      catchError(this.handleError)
+    );
   }
 
   createPost(post: any): Observable<any> {
@@ -30,25 +48,40 @@ export class ApiClientService {
   }
 
   updatePost(id: number, post: any): Observable<any> {
+    delete this.cache[`post-${id}`];
+
     return this.http
       .put(`${this.apiUrl}/posts/${id}`, post)
       .pipe(catchError(this.handleError));
   }
 
+  // Delete a post
   deletePost(id: number): Observable<any> {
+    delete this.cache[`post-${id}`];
+
     return this.http
       .delete(`${this.apiUrl}/posts/${id}`)
       .pipe(catchError(this.handleError));
   }
 
   getComments(postId: number): Observable<any> {
+    const cacheKey = `comments-post-${postId}`;
+
+    if (this.cache[cacheKey]) {
+      return of(this.cache[cacheKey]);
+    }
     return this.http
       .get(`${this.apiUrl}/comments`, {
         params: { postId: postId.toString() },
       })
-      .pipe(retry(2), catchError(this.handleError));
+      .pipe(
+        retry(2),
+        tap((data) => (this.cache[cacheKey] = data)),
+        catchError(this.handleError)
+      );
   }
 
+  // Error handling
   private handleError(error: HttpErrorResponse) {
     let errorMsg = 'An unknown error occurred!';
     if (error.error instanceof ErrorEvent) {
